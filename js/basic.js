862579c937b6e0b939f313c5636e9dea759a5179
KyawThetOo007 <kthet4951@gmail.com>
2025-05-16 23:48:16 +0900

    add basicJs

---
+++ js/basic.js
@@ -0,0 +1,39 @@
+// let x;
+// let y;
+
+// x = 5;
+// y = 10;
+// console.log(x + y); // 15
+
+// const PI = 3.14;
+// const RADIUS = 5;
+// const area = PI * RADIUS * RADIUS;
+// console.log(area); // 78.5
+
+// let myName = "Kyaw Thet Oo";
+// let myAge = 28;
+// let myHobby = "Coding";
+// let myInfo = `My name is ${myName}, I am ${myAge} years old and I love ${myHobby}.`;
+// console.log(myInfo); // My name is Kyaw Thet Oo, I am 28 years old and I love Coding.
+
+const fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+
+fruits[3] = "Mango"; // Change "Cherry" to "Mango"
+
+console.log(fruits); // ["Apple", "Banana", "Mango", "Date", "Elderberry"]
+console.log(fruits[0]); // "Apple"
+console.log(fruits[1]); // "Banana" 
+console.log(fruits[2]); // "Mango"
+console.log(fruits[3]); // "Date"
+console.log(fruits[4]); // "Elderberry"
+
+const arr =[];
+
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+arr[3] = 4;
+arr[4] = 5;
+
+console.log(arr); // [1, 2, 3, 4, 5]
+console.log(arr.length); // [1, 2, 3, 4, 5]
